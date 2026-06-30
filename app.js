56ef83105c92b3d3f14cd8420f5114fe5dd831f4
Abhiraj kumar <abhirajmait16@gmail.com>
2026-06-30 14:14:26 +0530

    Implemented listing CRUD operations and updated README

--- app.js
+++ app.js
@@ -2,28 +2,71 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const mongoUrl = "mongodb://127.0.0.1:27017/wanderlust";
 
 async function main() {
   await mongoose.connect(mongoUrl);
 }
-async function main() {
-  await mongoose.connect(mongoUrl);
-}
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/listings", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "my home",
-    description: "buy this villa",
-    price: 1200,
-    location: "Goa",
-    country: "Inida",
-  });
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("succefull");
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+app.post("/listings", async (req, res) => {
+  let newListig = new Listing(req.body.listing);
+  await newListig.save();
+  res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// app.get("/listings", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "my home",
+//     description: "buy this villa",
+//     price: 1200,
+//     location: "Goa",
+//     country: "Inida",
+//   });
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("succefull");
+// });
 
 main()
   .then(() => {
